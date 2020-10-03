eab9f2f79669790df190c30b27da9e1da33b8bdf
allfta <allfta@naver.com>
2020-10-03 22:48:41 +0700

    Updates

---
+++ precache-manifest.08708fe20c0fe4f2cd9a82b6f24b1be1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd476f4d9068d5b2ae7aa585e443c6e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ce0207e0f3a72ca32f03",
+    "url": "/movie_app/static/css/main.f5bbf4e1.chunk.css"
+  },
+  {
+    "revision": "890fdb3298edb943bfa3",
+    "url": "/movie_app/static/js/2.bb9b8681.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.bb9b8681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0207e0f3a72ca32f03",
+    "url": "/movie_app/static/js/main.a1d604a7.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
